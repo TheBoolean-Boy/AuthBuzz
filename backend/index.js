b854996f103971b1c5623df96b05df5b83c6d6c1
TheBoolean-Boy <saswatrath02@gmail.com>
2025-09-02 17:21:11 +0530

    dB Schema, routes setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("This is the Home Page");
-})
+const PORT = process.env.PORT || 5000;
 
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  connectDB();
+  console.log(`Server is running at PORT:${PORT}`);
 })
+
+
+
